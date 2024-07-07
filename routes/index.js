3dce7b297b49953b4659f063f2cf17dd430c874a
Trang T <trangmtruong@yahoo.com>
2024-07-07 14:24:42 -0700

    add empty lines after require statement

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 // Routes tell the app which controller to use when a user makes a request
 // like signposts that direct traffic
 const router = require("express").Router();
+
 const clothingRouter = require("./clothingItems");
+
 const userRouter = require("./users");
+
 const { NOT_FOUND, messageNotFoundError } = require("../utils/errors");
+
 router.use("/items", clothingRouter);
 router.use("/users", userRouter);
 
