7fda3dc48606b708a30a039022089e692e742530
Trang Truong <trangmtruong@Trangs-MacBook-Pro.attlocal.net>
2026-01-29 19:15:57 -0800

    clones to new laptop and installed npm, fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
 
-  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  extends: ["eslint:recommended", "prettier"],
   overrides: [
     {
       env: {
